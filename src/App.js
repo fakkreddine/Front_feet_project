267a900c71906d8f5eea86815e05bd9594b8c883
ahmed ksontini <57683675+ahmedksont@users.noreply.github.com>
2024-11-16 11:05:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Login from './layout/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from "./Auth/AuthContext";
-import Login from './layout/Login';
 import Home from './layout/Home';
 import Signup from './layout/Signup';
 import ProtectRoute from "./Auth/ProtectRoute";
 import UserHome from './layout/UserHome';
-import TeacherPage from './layout/TeacherPage'; 
-import DepartmentPage from './layout/DepartmentPage'; 
+
+import Dash from './component/Dash';
 import SchedulePage from './layout/SchedulePage';
+
+import Aside from './component/Aside';
+import DepartmentPage from './layout/DepartmentPage'; 
+
 
 function App() {
   return (
     <div className="App">
+     
+
       <AuthProvider>
         <BrowserRouter>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/home" element={<ProtectRoute><Home /></ProtectRoute>} />
-            <Route path="/dash" element={<ProtectRoute><UserHome /></ProtectRoute>} />
-            <Route path="/teachers" element={<ProtectRoute><TeacherPage /></ProtectRoute>} />
-            <Route path="/students" element={<ProtectRoute><TeacherPage /></ProtectRoute>} />
-            <Route path="/departments" element={<ProtectRoute><DepartmentPage /></ProtectRoute>} />
-            <Route path="/schedule" element={<ProtectRoute><SchedulePage /></ProtectRoute>} />
+            
+            
+            
+            <Route path="/home" element={<ProtectRoute><Home></Home></ProtectRoute> }/>
+        <Route path='/dash' element={<ProtectRoute><UserHome></UserHome><Dash></Dash></ProtectRoute> }/>
+
+
+       
+        <Route path="/schedule" element={<ProtectRoute><SchedulePage /></ProtectRoute>} />
+       
+
+
+      
+            <Route path="/departments" element={<ProtectRoute><DepartmentPage /></ProtectRoute>} /> 
+          
           </Routes>
         </BrowserRouter>
       </AuthProvider>
+
+
+
+        
+        
+      
+      
+   
+
     </div>
   );
 }
